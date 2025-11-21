d30125a736a9819022e3437187bf6fdcbee22df3
logic_roar <logic_roar@archlinux>
2025-11-21 11:07:13 +0100

    bla bla bla, here goes the commit

---
+++ src/main.py
@@ -0,0 +1,9 @@
+__version__ = "2025.11.21.1"
+if __name__ == "__main__":
+    print("L   III N N EEE N N")
+    print("L    I  NNN E   NNN")
+    print("L    I  NNN EEE NNN")
+    print("L    I  NNN E   NNN")
+    print("LLL III N N EEE N N")
+    print("\nversion " + __version__)
+
